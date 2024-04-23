fb4e19622fb58d8187396d98c4a224bb38262d2d
sinopdrivers <167090262+sinopdrivers@users.noreply.github.com>
2024-04-23 13:52:14 +0200

    Add files via upload

--- TubeFlash_files/public_html/db.php
+++ TubeFlash_files/public_html/db.php
@@ -1,3 +1,3 @@
 <?php
-$connect = mysqli_connect(mysql.serv00/ct8pl or any', 'yourusername', 'yourpassword', 'yourdbname') or die ("Database error!"); // db, do not remove or broken site.
+$connect = mysqli_connect('mysql.serv00/ct8pl or any', 'yourusername', 'yourpassword', 'yourdbname') or die ("Database error!"); // db, do not remove or broken site.
 ?>
