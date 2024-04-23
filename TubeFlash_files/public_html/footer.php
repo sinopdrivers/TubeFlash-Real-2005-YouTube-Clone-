b7791a497c646ed45fff386881393b7ae99558ef
sinopdrivers <167090262+sinopdrivers@users.noreply.github.com>
2024-04-23 13:56:01 +0200

    footer.php

--- TubeFlash_files/public_html/footer.php
+++ TubeFlash_files/public_html/footer.php
@@ -1,7 +1,7 @@
 <table cellpadding="10" cellspacing="0" border="0" align="center">
   <tr>
     <td align="center" valign="center">
-      <span class="footer"><a href="whats_new.php">What's New</a> | <a href="aboutus.php">About Us</a> | <a href="help.php">Help</a> | <a href="https://discord.com/invite/wJMBcH7SAE">Discord Server</a> | <a href="terms.php">Terms of Use</a> | <a href="privacy.php">Privacy Policy</a> 
+      <span class="footer"><a href="whats_new.php">What's New</a> | <a href="aboutus.php">About Us</a> | <a href="help.php">Help</a> | <a href="">Discord Server</a> | <a href="terms.php">Terms of Use</a> | <a href="privacy.php">Privacy Policy</a> 
       <br>
       <br>
       Copyright &copy; 2024 TubeFlash LLC</span></td>
